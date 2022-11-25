f94e433395c22e2c263dbe0466d46f29f140d74f
Alyson Consuli <consulialyson@gmail.com>
2022-11-25 17:18:59 -0300

    feat: mapper configuration

---
+++ App.API/Configurations/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using App.API.Data;
+using App.API.Models.Character;
+using App.API.Models.Country;
+using AutoMapper;
+
+namespace App.API.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Country, CreateCountryDto>().ReverseMap();
+            CreateMap<Country, GetCountryDto>().ReverseMap();
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Country, UpdateCountryDto>().ReverseMap();
+
+            CreateMap<Character, CharacterDto>().ReverseMap();
+        }
+    }
+}
